027ad6a09f1bd181c0e6e1cb8db01b14f4ae94b1
ValentinSaraiva-MMI <95092193+ValentinSaraiva-MMI@users.noreply.github.com>
2022-09-01 11:07:07 -0400

    ajout tailwind css + creat page produit secondaire

--- main.js
+++ main.js
@@ -1,45 +1,89 @@
 let app = function () {
-    return{
+    return {
 
 
-expos: [
-    {
-        img: "images/Ma_maison.jpeg",
-        nom: "Ma maison",
-        prix: "15$",
-        link: "",
-    },
-    {
-        img: "images/expo_pirates_corsaires.jpeg",
-        nom: "À l’abordage ! Pirates ou corsaires ?",
-        prix: "15$",
-        link: "",
-    },
-    {
-        img: "images/Pompei.jpeg",
-        nom: "Pompéi. Cité immortelle",
-        prix: "20$",
-        link: "",
-    },
-    {
-        img: "images/temps_quebecois.jpeg",
-        nom: "Le temps des Québécois",
-        prix: "18$",
-        link: "",
-    },
-    {
-        img: "images/Frederic_Back.jpeg",
-        nom: "FRÉDÉRIC BACK, passionné pour la planète",
-        prix: "18$",
-        link: "",
-    },
-    {
-        img: "images/Vikings.webp",
-        nom: "VIKINGS – Dragons des mers du Nord",
-        prix: "22$",
-        link: "",
-    }
+        expos: [
+            {
+                img: "images/Ma_maison.jpeg",
+                nom: "Ma maison",
+                prix: "15$",
+                link: "",
+            },
+            {
+                img: "images/expo_pirates_corsaires.jpeg",
+                nom: "À l’abordage ! Pirates ou corsaires ?",
+                prix: "15$",
+                link: "",
+            },
+            {
+                img: "images/Pompei.jpeg",
+                nom: "Pompéi. Cité immortelle",
+                prix: "20$",
+                link: "",
+            },
+            {
+                img: "images/temps_quebecois.jpeg",
+                nom: "Le temps des Québécois",
+                prix: "18$",
+                link: "",
+            },
+            {
+                img: "images/Frederic_Back.jpeg",
+                nom: "FRÉDÉRIC BACK, passionné pour la planète",
+                prix: "18$",
+                link: "",
+            },
+            {
+                img: "images/Vikings.webp",
+                nom: "VIKINGS – Dragons des mers du Nord",
+                prix: "22$",
+                link: "",
+            }
 
         ],
+
+
+
+        extras: [
+            {
+                img: "images/collation.jpeg",
+                nom: "Collation",
+                prix: "15$",
+
+            },
+            {
+                img: "images/repas.jpeg",
+                nom: "Repas",
+                prix: "35$",
+
+            },
+            {
+                img: "images/album.jpeg",
+                nom: "Album n°1",
+                prix: "25$",
+
+            },
+            {
+                img: "images/album_02.jpeg",
+                nom: "Albumn n°2",
+                prix: "25$",
+
+            },
+            {
+                img: "images/coffret.jpeg",
+                nom: "Coffret n°1",
+                prix: "58$",
+
+            },
+            {
+                img: "images/coffret_02.jpeg",
+                nom: "Coffret n°2",
+                prix: "58$",
+
+            },
+
+
+        ],
+
     }
 }

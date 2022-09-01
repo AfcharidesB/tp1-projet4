bb6af63f2022cd0d3e5b6acd6e022ef9470a5191
Pierre Liger <pierre.liger4@gmail.com>
2022-09-01 09:51:51 -0400

    Pierre comit 1

---
+++ main.js
@@ -0,0 +1,45 @@
+let app = function () {
+    return{
+
+
+expos: [
+    {
+        img: "images/Ma_maison.jpeg",
+        nom: "Ma maison",
+        prix: "15$",
+        link: "",
+    },
+    {
+        img: "images/expo_pirates_corsaires.jpeg",
+        nom: "À l’abordage ! Pirates ou corsaires ?",
+        prix: "15$",
+        link: "",
+    },
+    {
+        img: "images/Pompei.jpeg",
+        nom: "Pompéi. Cité immortelle",
+        prix: "20$",
+        link: "",
+    },
+    {
+        img: "images/temps_quebecois.jpeg",
+        nom: "Le temps des Québécois",
+        prix: "18$",
+        link: "",
+    },
+    {
+        img: "images/Frederic_Back.jpeg",
+        nom: "FRÉDÉRIC BACK, passionné pour la planète",
+        prix: "18$",
+        link: "",
+    },
+    {
+        img: "images/Vikings.webp",
+        nom: "VIKINGS – Dragons des mers du Nord",
+        prix: "22$",
+        link: "",
+    }
+
+        ],
+    }
+}

2e1d91bfed9fa64b712aecff79b6f6b5f5c1d455
unknown <afcharides08@gmail.com>
2022-09-05 10:15:49 -0400

    ajout du logo sur tout les pages

---
+++ interact.js
@@ -0,0 +1,18 @@
+
+const expoSection = document.querySelector(".section-exposition");
+
+expoSection.addEventListener("click", openExpo)
+function openExpo() {
+    window.location.href = "page_expo.html";
+    
+}
+
+const secondary = document.querySelector(".section-secondaire");
+
+secondary.addEventListener("click", openSecondary)
+
+function openSecondary() {
+    window.location.href = "page_produit_squelette.html";
+    
+
+}
